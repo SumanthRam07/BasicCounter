c518ea8c6331612ce903660d5da90170d65ad526
SumanthRam07 <sumanthgollaprolu07@gmail.com>
2024-03-26 23:25:49 -0400

    My first counter commit

---
+++ src/counter/Counter.jsx
@@ -0,0 +1,124 @@
+import { useContext, useState } from "react"
+import {Proptypes} from 'prop-types'
+
+import './Counter.css'
+
+export default function Counter()
+
+{
+
+   const[sum , SumAdder] = useState(0)
+
+   function ParentIncrememt2(by)
+   {
+
+        SumAdder(sum+by) 
+
+     
+      
+
+
+   }
+   function ParentDecrememt(by)
+   {
+
+        SumAdder(sum-by) 
+
+
+   }
+
+   function Reset()
+   {
+    SumAdder(0)
+
+   }
+
+
+
+    return(
+
+    <div>
+      <span className="button">{sum}</span>
+      {sum <= 0 && <div style={{ color: 'red' }}>Warning: The sum has gone below 0</div>}
+
+      
+    <BasicCounter  by = {1}  ParentIncrememt1= {ParentIncrememt2} ParentDecrememt= {ParentDecrememt} > </BasicCounter>
+    <BasicCounter  by = {2} ParentIncrememt1= {ParentIncrememt2} ParentDecrememt= {ParentDecrememt}> </BasicCounter>
+    <BasicCounter  by = {3}  ParentIncrememt1= {ParentIncrememt2} ParentDecrememt= {ParentDecrememt}> </BasicCounter>
+    <BasicCounter  by = {4}  ParentIncrememt1= {ParentIncrememt2} ParentDecrememt= {ParentDecrememt} ></BasicCounter>
+    <BasicCounter  by = {5} ParentIncrememt1= {ParentIncrememt2} ParentDecrememt= {ParentDecrememt}> </BasicCounter>
+
+    <div>
+
+             <button className="Button" onClick={Reset}>RESET </button>
+
+             </div>
+
+
+
+    </div>
+
+
+    )
+}
+
+
+
+    export  function  BasicCounter({by , ParentIncrememt1 , ParentDecrememt})
+{
+
+
+     
+    return( 
+
+
+         <div  className="Login"> 
+         
+         <div>
+         
+         </div>
+      
+         <div className="Increment Button "> 
+
+           <div>
+             
+
+            
+             <div>
+               
+   
+             <button className="Button" onClick={ ()=> ParentIncrememt1(by)} >+{by}</button>
+             <button className="Button" onClick={()=>  ParentDecrememt(by)} >-{by}</button>
+
+    
+             
+
+             </div>
+             
+
+
+
+
+           
+           </div>
+
+
+
+         </div>
+         
+         
+         
+         
+         
+             </div>
+    )
+
+     
+
+
+ 
+    
+
+
+}
+
