4e76852c358abe3495264c7a401192147a9c5642
SumanthRam07 <sumanthgollaprolu07@gmail.com>
2024-03-27 01:37:52 -0400

    hopefully this is the last time

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Counter from './counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+<header> Welcome to counter app testing</header> 
+
+    <div>
+
+      <Counter  > </Counter>
+   
+      
+   
+    </div>
+    </div>
+  );
+}
+
+export default App;
